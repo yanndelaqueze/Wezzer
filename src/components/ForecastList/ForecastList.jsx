a6f41550a8970ac4630be5eeea8888497fe06c52
Yann Delaqueze <yann.delaqueze@gmail.com>
2024-01-09 15:17:30 +0100

    fcst list api

--- src/components/ForecastList/ForecastList.jsx
+++ src/components/ForecastList/ForecastList.jsx
@@ -1,7 +1,24 @@
 import s from "./style.module.css";
+import { useEffect, useState } from "react";
 import { ForecastListItem } from "../ForecastListItem/ForecastListItem";
+import { OpenWeatherAPI } from "../../api/openweather";
 
 export function ForecastList({ selectedCity }) {
+  const [forecastList, setforecastList] = useState();
+
+  async function getWeatherForecast(city) {
+    const res = await OpenWeatherAPI.getWeather(city.lat, city.lng);
+    setforecastList(res.data.list);
+  }
+
+  useEffect(() => {
+    if (selectedCity) {
+      getWeatherForecast(selectedCity);
+    }
+  }, [selectedCity]);
+
+  console.log("fcst list :", forecastList);
+
   return (
     <div>
       <span>
