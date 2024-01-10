61eff8fd416bf8f92e1e1f7e7a157536ffed4eba
Yann Delaqueze <yann.delaqueze@gmail.com>
2024-01-10 13:32:39 +0100

    scroll to selected City

--- src/components/CityList/CityList.jsx
+++ src/components/CityList/CityList.jsx
@@ -2,6 +2,7 @@ import s from "./style.module.css";
 import { CityListItem } from "../CityListItem/CityListItem";
 import { ArrowClockwise, Geo } from "react-bootstrap-icons";
 import { Pin } from "react-bootstrap-icons";
+import { useRef, useEffect } from "react";
 
 export function CityList({
   selectedCity,
@@ -10,6 +11,19 @@ export function CityList({
   onClickTrash,
   onClickCity,
 }) {
+  const selectedCityRef = useRef(null);
+
+  useEffect(() => {
+    // Scroll to selectedCity when it changes
+    if (selectedCityRef.current) {
+      selectedCityRef.current.scrollIntoView({
+        behavior: "smooth",
+        block: "nearest",
+        inline: "start",
+      });
+    }
+  }, [selectedCity]);
+
   return (
     <>
       <div className={s.list_title}>
@@ -38,7 +52,10 @@ export function CityList({
         {/* DISPLAY OTHER CITIES FROM placeList */}
         {placeList.map((city, i) => {
           return (
-            <span className={s.city_item}>
+            <span
+              className={s.city_item}
+              ref={city === selectedCity ? selectedCityRef : null}
+            >
               {city && (
                 <CityListItem
                   key={city + i}
