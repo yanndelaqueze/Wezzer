057d21e64c267798306a915e0f764556adc15c44
Yann Delaqueze <yann.delaqueze@gmail.com>
2024-01-12 11:46:34 +0100

    api key

--- src/api/geocoder.js
+++ src/api/geocoder.js
@@ -19,5 +19,5 @@ export class GeocoderAPI {
 }
 
 // https://api.mapbox.com/geocoding/v5/{endpoint}/{longitude},{latitude}.json
-// https://api.mapbox.com/geocoding/v5/mapbox.places/2.3920276,48.8542194.json?types=place&access_token=pk.eyJ1IjoieWFubmRlbGFxdWV6ZSIsImEiOiJjbGR5a2NueTIwMDJrM25vOXBhaXdiNHJuIn0.LeeRjGt1japvv4d5yvaRKw
-// https://api.mapbox.com/geocoding/v5/mapbox.places/nante.json?types=place&limit=5&access_token=pk.eyJ1IjoieWFubmRlbGFxdWV6ZSIsImEiOiJjbGR5a2NueTIwMDJrM25vOXBhaXdiNHJuIn0.LeeRjGt1japvv4d5yvaRKw
+// https://api.mapbox.com/geocoding/v5/mapbox.places/2.3920276,48.8542194.json?types=place&access_token={API_KEY}
+//
